c1755735fae759fd8240d434f31dfe0eb1f703a8
dmoore5050 <dustinmoore@gmail.com>
2013-05-15 13:10:55 -0500

    deleted unused saison.rb file

--- test/test_adding_project.rb
+++ test/test_adding_project.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
+# require_relative '../models/project'
 
 class TestAddingProject < Test::Unit::TestCase
+
   def test_takes_arguments_and_saves_them
     # start with no projects
     assert_equal Project.count, 0
@@ -9,4 +11,5 @@ class TestAddingProject < Test::Unit::TestCase
     # end up with a 'foo' project
     assert_equal Project.count, 1
   end
+
 end
