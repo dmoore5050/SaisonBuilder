b38c01348a6c30b0f2e318d6a5a573b75b29646a
dmoore5050 <dustinmoore@gmail.com>
2013-05-18 09:23:08 -0500

    implemented rest of Eliza's base code

--- test/test_adding_project.rb
+++ test/test_adding_project.rb
@@ -1,16 +1,32 @@
 require 'test_helper'
 # require_relative '../models/project'
 
-class TestAddingProject < Test::Unit::TestCase
+class TestAddingProject < MiniTest::Unit::TestCase
+  include DatabaseCleaner
+
 
   def test_takes_arguments_and_saves_them
-    # start with no projects
-    assert_equal Project.count, 0
-    # `ruby futureperfect add foo`
-    project = Project.create( name: 'foo' )
-    # project => #<Project:12312 @name='foo'>
-    # end up with a 'foo' project
-    assert_equal Project.count, 1
+    assert_equal 0, Project.count
+    `ruby futureperfect add foo`
+    assert_equal 1, Project.count
+  end
+
+  def test_takes_arguments_and_uses_them
+    `ruby futureperfect add foo`
+    assert_equal 'foo', Project.last.name
+  end
+
+  def test_duplicate_names_are_ignored
+    Project.create( name: 'foo' )
+    original_project_count = Project.count
+    `ruby futureperfect add foo`
+    assert_equal original_project_count, Project.count
+  end
+
+  def test_duplicate_names_outputs_error_message
+    Project.create( name: 'foo' )
+    results = `ruby futureperfect add foo`
+    assert results.include?('Name must be unique'), "Actually was '#{results}'"
   end
 
 end
