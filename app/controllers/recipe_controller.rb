5e8f67cb570873cc27e2577975fa52e2f2c6ec08
dmoore5050 <dustinmoore@gmail.com>
2013-05-21 21:53:12 -0500

    added recipe_controller file

---
+++ app/controllers/recipe_controller.rb
@@ -0,0 +1,35 @@
+class RecipeController
+
+  def initialize params
+    @params = params
+  end
+
+  def create
+    recipe = Recipe.new(params[:recipe])
+    case
+    when recipe.save then puts 'Success!'
+    else puts "Failure: #{recipe.errors.full_messages.join(", ")}"
+    end
+  end
+
+  def list_recipes
+    recipes = Recipe.all
+    recipes.each_with_index do |recipe, i|
+      puts "#{i+1}. #{recipe.name}"
+    end
+  end
+
+  def delete
+    matching_recipes = Recipe.where(name: params[:recipe][:name]).all
+    matching_recipes.each do |recipe|
+      recipe.destroy
+    end
+  end
+
+  private
+
+  def params
+    @params
+  end
+
+end
