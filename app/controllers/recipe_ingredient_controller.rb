89c0ed0213a2c01c656367c04d5f23cfa8ddb300
dmoore5050 <dustinmoore@gmail.com>
2013-05-23 08:25:14 -0500

    add recipe_ingredient controller

---
+++ app/controllers/recipe_ingredient_controller.rb
@@ -0,0 +1,25 @@
+class RecipeIngredientController
+
+  def initialize
+  end
+
+  def create
+    recipe_ingredient = RecipeIngredient.new(params[:recipe_ingredient])
+    case
+    when recipe_ingredient.save then puts 'Success!'
+    else puts "Failure: #{recipe_ingredient.errors.full_messages.join(", ")}"
+    end
+  end
+
+  def view
+    'show recipe here'
+  end
+
+  def delete
+    matching_recipe_ingredients = RecipeIngredient.where(name: params[:recipe_ingredient][:name]).all
+    matching_recipe_ingredients.each do |recipe|
+      recipe.destroy
+    end
+  end
+
+end
