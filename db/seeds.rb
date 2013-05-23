0d81e237dd8bc0412937f93686dc2381a1c37a5a
dmoore5050 <dustinmoore@gmail.com>
2013-05-23 14:18:27 -0500

    added seeding array. working on seeding loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,10 @@ connect_to 'development'
 
 ingredient_list = [
   ['Pilsner', 'Pale base malt, prominent in continental examples.', 'Grain'],
-  ['2-row Pale Malt', 'Pale base malt, commonly used in American examples.', 'Grain'],
+  ['Pale Malt', 'Pale base malt, commonly used in American examples.', 'Grain'],
   ['Munich', 'Base malt, adds richness. Use to supplement wheat, pilsner, or pale malt.', 'Grain'],
   ['White Wheat Malt', 'Base malt, added to modify malt character. Doughy, mild.', 'Grain'],
-  ['Caramunich 60', 'Caramel malt, adds mild caramel/toffee flavors and sweetness.', 'Grain'],
+  ['Caramunich', 'Caramel malt, adds mild caramel/toffee flavors and sweetness.', 'Grain'],
   ['Honey Malt', 'Specialty malt. Adds distinctive honey-like sweetness.', 'Grain'],
   ['Chocolate Malt', 'Kilned specialty malt. Adds nutty, chocolate flavor and color.', 'Grain'],
   ['Carafa 2 Special', 'Kilned specialty malt. Adds color, mild roasty flavors w/o harshness.', 'Grain'],
@@ -45,65 +45,85 @@ ingredient_list.each do | name, description, type_code, yeast_code_wl, yeast_cod
 end
 
  # ROUGH DRAFT
-# recipe_array = [
-#   [
-#     [name, boil_length, ferm_temp],
-#     [
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration]
-#     ]
-#   ],
-#   [
-#     [name, boil_length, ferm_temp],
-#     [
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration]
-#     ]
-#   ],
-#   [
-#     [name, boil_length, ferm_temp],
-#     [
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration]
-#     ]
-#   ],
-#   [
-#     [name, boil_length, ferm_temp],
-#     [
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration]
-#     ]
-#   ],
-#   [
-#     [name, boil_length, ferm_temp],
-#     [
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration],
-#       [name, usage, quantity, duration]
-#     ]
-#   ]
-# ]
+recipe_array = [
+  [
+    ['classic', 90, '85F'], # iteration problem
+    [
+      ['pilsner', nil, 9, nil],
+      ['munich', nil, 1, nil],
+      ['corn sugar', 'peak krausen', 1, nil],
+      ['styrian goldings', 'boil', 1.5, '60 min'],
+      ['saaz', 'boil', 0.5, '15 min'],
+      ['dupont', 'primary', nil, nil, 565, 3724]
+    ]
+  ],
+  [
+    ['hoppy classic', 90, '85F'],
+    [
+      ['pilsner', nil, 9, nil],
+      ['white wheat malt', nil, 0.5, nil],
+      ['munich', nil, 0.5, nil],
+      ['corn sugar', 'peak krausen', 1, nil],
+      ['styrian goldings', 'boil', 1.3, '60 min'],
+      ['hallertau', 'boil', 1, '15 min'],
+      ['styrian goldings', 'boil', 0.7, '1 min'],
+      ['saaz', 'dryhop', 1, '5 days'],
+      ['dupont', 'primary', nil, nil, 565, 3724]
+    ]
+  ],
+  [
+    ['rye saison', 90, '65-72F'],
+    [
+      ['pilsner', nil, 6, nil],
+      ['rye malt', nil, 3, nil],
+      ['munich', nil, 1, nil],
+      ['corn sugar', 'peak krausen', 1, nil],
+      ['hallertau', 'boil', 1.75, '60 min'],
+      ['hallertau', 'boil', 1, '1 min'],
+      ['french', 'primary', nil, nil, 656, 3711]
+    ]
+  ],
+  [
+    ['new world', 60, '65-72F'],
+    [
+      ['pale malt', nil, 7, nil],
+      ['white wheat malt', nil, 2, nil],
+      ['munich', nil, 1, nil],
+      ['turbinado sugar', 'peak krausen', 1, nil],
+      ['hallertau', 'boil', 1.5, '60 min'],
+      ['amarillo', 'boil', 1, '10 min'],
+      ['motueka', 'boil', 1, '1 min'],
+      ['amarillo', 'dryhop', 1, '5 days'],
+      ['simcoe', 'dryhop', 1, '5 days'],
+      ['french', 'primary', nil, nil, 656, 3711]
+    ]
+  ],
+  [
+    ['black saison', 60, '65-72F'],
+    [
+      ['pale malt', nil, 8, nil],
+      ['munich', nil, 2, nil],
+      ['carafa 2 special', nil, 0.4, nil],
+      ['chocolate malt', nil, 0.4, nil],
+      ['hallertau', 'boil', 1.5, '60 min'],
+      ['american farmhouse', 'primary', nil, nil, 670],
+    ]
+  ]
+]
 
-# recipe_array.each do | recipe |
-#   recipe.each do | recipe_args, recipe_ingredient_args |
-#     recipe = Recipe.create(name, boil_length, ferm_temp)
-#     recipe.save
-#     ri_args.each do | name, usage, quantity, duration |
-#       recipe_ingredient = recipe.recipe_ingredients.create(usage, quantity, duration)
-#       ingr = Ingredient.where(name: params[:ingredient][:name]).first
-#       recipe_ingredient.ingredient = ingr
-#       recipe_ingredient.save
-#     end
-#   end
-# end
+recipe_array.each do | recipe |
+  recipe.each do
+    name, boil_length, ferm_temp = recipe[0][0], recipe[0][1], recipe[0][2]
+    the_recipe = Recipe.create(name: name, boil_length: boil_length, primary_fermentation_temp: ferm_temp)
+    the_recipe.save
+    recipe[1].each_with_index do | name, usage, quantity, duration, yeast_code_wl, yeast_code_wyeast |
+      puts the_recipe
+      the_recipe_ingredient = the_recipe.recipe_ingredient.create(usage: usage, quantity: quantity, duration: duration)
+      ingr = Ingredient.where(name: params[:ingredient][:name]).first
+      the_recipe_ingredient.ingredient = ingr
+      the_recipe_ingredient.save
+    end
+  end
+end
 
 
