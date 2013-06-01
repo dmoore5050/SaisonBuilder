42bb33208af47ec7cb17231f03d623097ec52ea1
dmoore5050 <dustinmoore@gmail.com>
2013-06-01 08:04:52 -0500

    added addl ingr/recipe validation

--- test/integration/test_adding_ingredients.rb
+++ test/integration/test_adding_ingredients.rb
@@ -28,4 +28,26 @@ class TestAddingIngredient < MiniTest::Unit::TestCase
     assert results.include?(' is already assigned'), "Actually was '#{results}'"
   end
 
+  def test_blank_ingredient_name_is_ignored
+    original_ingredient_count = Ingredient.count
+    `ruby saisonbuilder add ingredient`
+    assert_equal original_ingredient_count, Ingredient.count
+  end
+
+  def test_blank_ingredient_name_outputs_error_message
+    results = `ruby saisonbuilder add ingredient`
+    assert results.include?(' is required to create a new Ingredient'), "Actually was '#{results}'"
+  end
+
+  def test_ingredient_name_longer_than_20_chars_is_ignored
+    original_ingredient_count = Ingredient.count
+    `ruby saisonbuilder add ingredient thisisareallylongingredientname`
+    assert_equal original_ingredient_count, Ingredient.count
+  end
+
+  def test_ingredient_name_longer_than_20_chars_outputs_error_message
+    results = `ruby saisonbuilder add ingredient thisisareallylongingredientname`
+    assert results.include?(' length must be 20 or fewer characters'), "Actually was '#{results}'"
+  end
+
 end
--- test/integration/test_adding_recipe.rb
+++ test/integration/test_adding_recipe.rb
@@ -27,4 +27,26 @@ class TestAddingRecipe < MiniTest::Unit::TestCase
     assert results.include?(' is already assigned'), "Actually was '#{results}'"
   end
 
+  def test_blank_name_is_ignored
+    original_recipe_count = Recipe.count
+    `ruby saisonbuilder add recipe`
+    assert_equal original_recipe_count, Recipe.count
+  end
+
+  def test_blank_name_outputs_error_message
+    results = `ruby saisonbuilder add recipe`
+    assert results.include?(' is required to create a new Recipe'), "Actually was '#{results}'"
+  end
+
+  def test_name_longer_than_20_chars_is_ignored
+    original_recipe_count = Recipe.count
+    `ruby saisonbuilder add recipe thisisareallylongrecipename`
+    assert_equal original_recipe_count, Recipe.count
+  end
+
+  def test_name_longer_than_20_chars_outputs_error_message
+    results = `ruby saisonbuilder add recipe thisisareallylongrecipename`
+    assert results.include?(' length must be 20 or fewer characters'), "Actually was '#{results}'"
+  end
+
 end
