9a28e09c700615293c98edab7c4be41fc3edef86
dmoore5050 <dustinmoore@gmail.com>
2013-06-01 16:04:21 -0500

    Renamed router

--- app/lib/router.rb
+++ app/lib/router.rb
@@ -4,7 +4,7 @@ RECIPE_NAME_ARRAY = %w( classic hoppy_classic rye_saison new_world black_saison 
 OPTION_ARRAY = %w( menu remove list_recipes modify yeast primary blend brett_only brett_secondary grain sweetness roast brown black wheat rye hops flavor aroma gravity increase decrease other spices fruit botanicals adjuncts more_botanicals)
 COMPONENTS_ARRAY = %w( dupont french american blend_brett_c blend_brett_b blend_brett_l only_brett_c only_brett_b only_brett_l only_brett_b_trois secondary_brett_b secondary_brett_c secondary_brett_l caramel honey wheat rye brown black eight seven five four bittering floral_spicy piney_citrus spicy floral citrus coriander citrus_zest white_peppercorns thai_basil ginger peaches blackberries mango currants hibiscus lavender rose_hips corn_sugar turbinado_sugar rice)
 
-module QuestionRouter
+module Router
 
   def self.route(question, trackback, record)
     puts question
@@ -24,7 +24,7 @@ module QuestionRouter
       selected_recipe = RecipeController.new params
       selected_recipe.view
     elsif COMPONENTS_ARRAY.include? answer
-      component = RecipeModification.new record
+      component = RecordModification.new record
       component.send("#{answer}")
     else
       puts "\n'#{answer}' is not a valid option. Please choose from the choices listed."
