98e87c5792e1125eae5002b64760cf653c105bd9
dmoore5050 <dustinmoore@gmail.com>
2013-06-03 13:44:07 -0500

    moved array constants to separate file

--- app/lib/router.rb
+++ app/lib/router.rb
@@ -1,9 +1,5 @@
 require_relative '../../bootstrap_ar'
 
-RECIPE_NAME_ARRAY = %w( classic hoppy_classic rye_saison new_world black_saison pacific_6_grain)
-OPTION_ARRAY = %w( menu remove modify yeast primary blend brett_only brett_secondary grain sweetness roast brown black wheat rye hops flavor aroma gravity increase decrease other spices fruit botanicals adjuncts more_botanicals)
-COMPONENTS_ARRAY = %w( dupont french american blend_brett_c blend_brett_b blend_brett_l only_brett_c only_brett_b only_brett_l only_brett_b_trois secondary_brett_b secondary_brett_c secondary_brett_l caramel honey wheat rye brown black eight seven five four bittering floral_spicy piney_citrus spicy floral citrus coriander citrus_zest white_peppercorns thai_basil ginger peaches blackberries mango currants hibiscus lavender rose_hips corn_sugar turbinado_sugar rice)
-
 module Router
 
   def self.route(question, trackback, record)
